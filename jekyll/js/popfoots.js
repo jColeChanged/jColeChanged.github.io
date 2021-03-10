55437e4552b4ddb05aefaa53d129291c09ec9d6b
Joshua Cole <jcolechanged@gmail.com>
2021-03-10 12:25:57 -0800

    Give popnotes some room to breathe and more congruent coloring.
    Popnotes were showing up in gray. This made the notes jarring.
    This fixes that issue by making them colored similar to the rest
    of the page. It also refactors them a bit; they are now decidedly
    not mobile friendly and very much take advantage of a large window.
    
    Verified that the popup notes are now much more pleasing.

--- jekyll/js/popfoots.js
+++ jekyll/js/popfoots.js
@@ -75,30 +75,32 @@ $(document).ready(function() {
         if(sup['id'] && sup['id'].substr(0,3) == 'fnr') {
             var footnr = sup['id'].substr(3);
             var footnote = document.getElementById('fn'+footnr);
-            if(!footnote) continue;
+            if(!footnote) {
+                continue;
+            }
             footnotehtml[i] = footnote.innerHTML;
             sup.setAttribute('footnoteindex',i);
             sup.onmouseover = function(event) {
                 var footnotepopup = document.getElementById('footnotepopup');
-                if(footnotepopup) footnotepopup.parentNode.removeChild(footnotepopup);
+                if(footnotepopup) {
+                    footnotepopup.parentNode.removeChild(footnotepopup);
+                }
                 var index = parseInt(this.getAttribute('footnoteindex'));
                 var popup = document.createElement('div');
                 popup.innerHTML = footnotehtml[index];
                 popup.id = 'footnotepopup';
-                popup.style.position = 'absolute';
-                popup.style.left = (event.pageX - 125) + 'px';
-                popup.style.top = (event.pageY + 25) + 'px';
-                popup.style.width = '250px';
-                popup.style.textAlign = 'left';
-                popup.style.backgroundColor = '#4a525a';
-                popup.style.border = '1px solid #636363';
-                popup.style.padding = '10px';
+                popup.style.left = (event.pageX) + 'px';
+                popup.style.top = (event.pageY) + 'px';
+
                 
                 document.body.appendChild(popup);
+                popup.classList.add("footnotepopup")
             };
             sup.onmouseout = function(event) {
                 var footnotepopup = document.getElementById('footnotepopup');
-                if(footnotepopup) footnotepopup.parentNode.removeChild(footnotepopup);
+                if(footnotepopup) {
+                    footnotepopup.parentNode.removeChild(footnotepopup);
+                }
             };
         }
     }
